63192414bb17b97a1c637a3f75fa0d7935641649
Samuel Aiudi-Murphy <doctoreeevil@gmail.com>
2020-11-05 13:35:10 -0800

    refactor player as a subclass of entity

--- game.js
+++ game.js
@@ -14,25 +14,36 @@ var twoDee = canvas.getContext("2d");
 twoDee.fillStyle = "red";
 twoDee.fillRect(0,0,canvas.width,canvas.height);
 
-function draw(time) {
+function tick(time) {
 //  var y1 = Math.floor(time) % (canvas.height);
 //  console.log({time, y1});
   twoDee.fillStyle = "white";
   twoDee.fillRect(0,0,canvas.width,canvas.height);
+  player.updatePosition();
   player.draw();
-  requestAnimationFrame(draw);
+  requestAnimationFrame(tick);
 }
 
-class Player {
-  constructor(x=50, y=50, vx=0, vy=0) {
+class Entity {
+  constructor(x=0, y=0, vx=0, vy=0) {
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
-    this.frames=0;
     this.lastTime = Date.now();
     this.radius = 10;
   }
+  updatePosition() {
+    var timeDelta = Date.now() - this.lastTime;
+    this.lastTime = Date.now();
+    var dx = timeDelta * this.vx;
+    var dy = timeDelta * this.vy;
+    this.y = Math.max(Math.min(this.y + dy, canvas.height-this.radius), this.radius);
+    this.x = Math.max(Math.min(this.x + dx, canvas.width-this.radius), this.radius);
+  }
+}
+
+class Player extends Entity {
   walk(walkDir, walkOrStop) {
     var walkOrStopInt = walkOrStop ? 1 : 0;
     var vscale = 0.5 * walkOrStopInt; // pixels per millisecond
@@ -55,21 +66,11 @@ class Player {
     //    console.log({walkDir, walkOrStop, vx, vy});
   }
   draw() {
-    this.updatePosition();
-    //    console.log(this);
     twoDee.fillStyle = "blue";
     twoDee.beginPath();
     twoDee.ellipse(this.x, this.y, this.radius, this.radius, 0, 0, 6.28, 0);
     twoDee.stroke();
     this.frames++;
-  }
-  updatePosition() {
-    var timeDelta = Date.now() - this.lastTime;
-    this.lastTime = Date.now();
-    var dx = timeDelta * this.vx;
-    var dy = timeDelta * this.vy;
-    this.y = Math.max(Math.min(this.y + dy, canvas.height-this.radius), this.radius);
-    this.x = Math.max(Math.min(this.x + dx, canvas.width-this.radius), this.radius);
   }
 }
 
@@ -98,4 +99,4 @@ function checkKeyEvent(event) {
 }
 document.addEventListener('keydown', checkKeyEvent);
 document.addEventListener('keyup', checkKeyEvent);
-requestAnimationFrame(draw);
+requestAnimationFrame(tick);
